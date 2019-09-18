c023e7766120102d14d7a6c8a9c87679f6d9983a
hcavanaugh <49169008+hcavanaugh@users.noreply.github.com>
2019-09-18 16:05:13 -0400

    Create deployer.php

---
+++ deployer.php
@@ -0,0 +1,32 @@
+<?php
+use \WP_CLI;
+use \WP_CLI_Command;
+/**
+ * Quick start new WP install. 
+ * Run WP-CLI Command to bulk install and activate plugins
+ * Install GeneratePress and child, activate child
+ **/
+class Command extends WP_CLI_Command {
+    public static $developer_plugins = [
+        'yoast',
+        'advanced-custom-fields', 
+        'classic-editor', 
+        'autoptimize',
+        'duplicate-post',
+        'https://github.com/elementor/elementor/archive/master.zip',
+        'https://github.com/wp-premium/gravityforms/archive/master.zip',
+        'user-role-editor',
+        'wp-sitemap-page'
+    ];
+
+
+    protected function enable_dev_plugins() {
+        WP_CLI::runcommand( 'plugin install ' . implode( ' ', self::$developer_plugins ). ' --activate --force' );
+    }
+
+    protected function enable_themes() {
+        WP_CLI::runcommand( 'wp theme install generatepress --force' )
+        WP_CLI::runcommand( 'wp theme install http://generatepress.com/api/themes/generatepress_child.zip --activate --force' )
+    }
+
+}
